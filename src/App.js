95f4e3cf6dc7a671378777120b5b7c80a8a8e3ea
Joe Hughes <joe.hughes.code@gmail.com>
2021-04-09 08:32:34 +0100

    Start roughing out first component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Stopwatch from './Stopwatch.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Stopwatch />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/Stopwatch.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Stopwatch extends React.Component {
+    render() {
+        return (
+            <div id="buttons">
+                <button>Start</button>
+                <button>Stop</button>
+            </div>
+        );
+    }
+}
+
+export default Stopwatch;
